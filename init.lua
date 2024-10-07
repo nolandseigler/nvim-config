3991eb34096f3d46b32de832a7dbaec6c88cc464
nolandseigler <57370691+nolandseigler@users.noreply.github.com>
2024-10-07 11:01:04 -0400

    still slogging through setup

--- init.lua
+++ init.lua
@@ -673,7 +673,322 @@ require('lazy').setup({
       }
     end,
   },
+  { -- Test
+    'nvim-neotest/neotest',
+    event = 'VeryLazy',
+    dependencies = {
+      'nvim-neotest/nvim-nio',
+      'nvim-lua/plenary.nvim',
+      'antoinemadec/FixCursorHold.nvim',
+      'nvim-treesitter/nvim-treesitter',
+      { 'fredrikaverpil/neotest-golang', version = '*' }, -- Installation
+      'nvim-neotest/neotest-python',
+    },
+    config = function()
+      local go_config = { -- Specify configuration
+        go_test_args = {
+          '-v',
+          '-race',
+          '-count=1',
+          --"-coverprofile=" .. vim.fn.getcwd() .. "/coverage.out",
+        },
+        testify_enabled = true,
+      }
+      local py_config = {
+        -- Extra arguments for nvim-dap configuration
+        -- See https://github.com/microsoft/debugpy/wiki/Debug-configuration-settings for values
+        dap = { justMyCode = false },
+        -- Command line arguments for runner
+        -- Can also be a function to return dynamic values
+        args = { '--log-level', 'DEBUG' },
+        -- Runner to use. Will use pytest if available by default.
+        -- Can be a function to return dynamic value.
+        runner = 'pytest',
+        -- Custom python path for the runner.
+        -- Can be a string or a list of strings.
+        -- Can also be a function to return dynamic value.
+        -- If not provided, the path will be inferred by checking for
+        -- virtual envs in the local directory and for Pipenev/Poetry configs
+        -- python = ".venv/bin/python",
+        -- Returns if a given file path is a test file.
+        -- NB: This function is called a lot so don't perform any heavy tasks within it.
+        -- !!EXPERIMENTAL!! Enable shelling out to `pytest` to discover test
+        -- instances for files containing a parametrize mark (default: false)
+        pytest_discover_instances = true,
+      }
+      require('neotest').setup {
+        adapters = {
+          require 'neotest-golang'(go_config), -- Registration
+          require 'neotest-python'(py_config),
+        },
+      }
+    end,
+    keys = {
+      {
+        '<leader>ta',
+        function()
+          require('neotest').run.attach()
+        end,
+        desc = '[t]est [a]ttach',
+      },
+      {
+        '<leader>tf',
+        function()
+          require('neotest').run.run(vim.fn.expand '%')
+        end,
+        desc = '[t]est run [f]ile',
+      },
+      {
+        '<leader>tA',
+        function()
+          require('neotest').run.run(vim.uv.cwd())
+        end,
+        desc = '[t]est [A]ll files',
+      },
+      {
+        '<leader>tS',
+        function()
+          require('neotest').run.run { suite = true }
+        end,
+        desc = '[t]est [S]uite',
+      },
+      {
+        '<leader>tn',
+        function()
+          require('neotest').run.run()
+        end,
+        desc = '[t]est [n]earest',
+      },
+      {
+        '<leader>tl',
+        function()
+          require('neotest').run.run_last()
+        end,
+        desc = '[t]est [l]ast',
+      },
+      {
+        '<leader>ts',
+        function()
+          require('neotest').summary.toggle()
+        end,
+        desc = '[t]est [s]ummary',
+      },
+      {
+        '<leader>to',
+        function()
+          require('neotest').output.open { enter = true, auto_close = true }
+        end,
+        desc = '[t]est [o]utput',
+      },
+      {
+        '<leader>tO',
+        function()
+          require('neotest').output_panel.toggle()
+        end,
+        desc = '[t]est [O]utput panel',
+      },
+      {
+        '<leader>tt',
+        function()
+          require('neotest').run.stop()
+        end,
+        desc = '[t]est [t]erminate',
+      },
+      {
+        '<leader>td',
+        function()
+          require('neotest').run.run { suite = false, strategy = 'dap' }
+        end,
+        desc = 'Debug nearest test',
+      },
+    },
+  },
+  -- DAP setup
+  {
+    'mfussenegger/nvim-dap',
+    event = 'VeryLazy',
+    --configurations = {
+    --python = {
+    --{
+    --type = 'python',
+    --request = 'launch',
+    --name = 'Launch file',
+    --program = '${file}',
+    --pythonPath = function()
+    --return '/usr/bin/python'
+    --end;
+    --},
+    --},
+    --},
+    keys = {
+      {
+        '<leader>db',
+        function()
+          require('dap').toggle_breakpoint()
+        end,
+        desc = 'toggle [d]ebug [b]reakpoint',
+      },
+      {
+        '<leader>dB',
+        function()
+          require('dap').set_breakpoint(vim.fn.input 'Breakpoint condition: ')
+        end,
+        desc = '[d]ebug [B]reakpoint',
+      },
+      {
+        '<leader>dc',
+        function()
+          require('dap').continue()
+        end,
+        desc = '[d]ebug [c]ontinue (start here)',
+      },
+      {
+        '<leader>dC',
+        function()
+          require('dap').run_to_cursor()
+        end,
+        desc = '[d]ebug [C]ursor',
+      },
+      {
+        '<leader>dg',
+        function()
+          require('dap').goto_()
+        end,
+        desc = '[d]ebug [g]o to line',
+      },
+      {
+        '<leader>do',
+        function()
+          require('dap').step_over()
+        end,
+        desc = '[d]ebug step [o]ver',
+      },
+      {
+        '<leader>dO',
+        function()
+          require('dap').step_out()
+        end,
+        desc = '[d]ebug step [O]ut',
+      },
+      {
+        '<leader>di',
+        function()
+          require('dap').step_into()
+        end,
+        desc = '[d]ebug [i]nto',
+      },
+      {
+        '<leader>dj',
+        function()
+          require('dap').down()
+        end,
+        desc = '[d]ebug [j]ump down',
+      },
+      {
+        '<leader>dk',
+        function()
+          require('dap').up()
+        end,
+        desc = '[d]ebug [k]ump up',
+      },
+      {
+        '<leader>dl',
+        function()
+          require('dap').run_last()
+        end,
+        desc = '[d]ebug [l]ast',
+      },
+      {
+        '<leader>dp',
+        function()
+          require('dap').pause()
+        end,
+        desc = '[d]ebug [p]ause',
+      },
+      {
+        '<leader>dr',
+        function()
+          require('dap').repl.toggle()
+        end,
+        desc = '[d]ebug [r]epl',
+      },
+      {
+        '<leader>dR',
+        function()
+          require('dap').clear_breakpoints()
+        end,
+        desc = '[d]ebug [R]emove breakpoints',
+      },
+      {
+        '<leader>ds',
+        function()
+          require('dap').session()
+        end,
+        desc = '[d]ebug [s]ession',
+      },
+      {
+        '<leader>dt',
+        function()
+          require('dap').terminate()
+        end,
+        desc = '[d]ebug [t]erminate',
+      },
+      {
+        '<leader>dw',
+        function()
+          require('dap.ui.widgets').hover()
+        end,
+        desc = '[d]ebug [w]idgets',
+      },
+    },
+  },
 
+  -- DAP UI setup
+  {
+    'rcarriga/nvim-dap-ui',
+    event = 'VeryLazy',
+    dependencies = {
+      'nvim-neotest/nvim-nio',
+      'mfussenegger/nvim-dap',
+    },
+    opts = {},
+    config = function(_, opts)
+      -- setup dap config by VsCode launch.json file
+      -- require("dap.ext.vscode").load_launchjs()
+      local dap = require 'dap'
+      local dapui = require 'dapui'
+      dapui.setup(opts)
+      dap.listeners.after.event_initialized['dapui_config'] = function()
+        dapui.open {}
+      end
+      dap.listeners.before.event_terminated['dapui_config'] = function()
+        dapui.close {}
+      end
+      dap.listeners.before.event_exited['dapui_config'] = function()
+        dapui.close {}
+      end
+    end,
+    keys = {
+      {
+        '<leader>du',
+        function()
+          require('dapui').toggle {}
+        end,
+        desc = '[d]ap [u]i',
+      },
+      {
+        '<leader>de',
+        function()
+          require('dapui').eval()
+        end,
+        desc = '[d]ap [e]val',
+      },
+    },
+  },
+  {
+    'theHamsta/nvim-dap-virtual-text',
+    opts = {},
+  },
   { -- Autoformat
     'stevearc/conform.nvim',
     event = { 'BufWritePre' },
